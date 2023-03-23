f7b91acf634d2d1e0a9561d19a08c8c02b0f459b
dingff <dingff0923@foxmail.com>
2023-03-23 13:47:27 +0800

    fix(type): add annotation

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -30,9 +30,28 @@ export type EntryObject = {
 };
 
 export type Options = {
+  /**
+   * 透传给 builtins.html，其中 chunks 和 excludedChunks 由插件控制，不允许修改
+   * 此处配置将作用于每个页面，因此 filename 不可用，可使用页面级配置修改
+   * 参考 https://www.rspack.dev/zh/config/builtins.html#builtinshtml
+   */
   html?: GlobalHtml;
+  /**
+   * 页面渲染时，挂载到节点的 id，默认是 'root'
+   */
   mountElementId?: string;
+  /**
+   * 可在此引入全局依赖
+   * 如: ['./src/global.scss']
+   */
   globalImport?: string[];
+  /**
+   * 是否将输出的 HTML 文件名转换为小写，默认为 false
+   */
   lowerCase?: boolean;
+  /**
+   * 布局组件的路径
+   * 如: './src/layout.jsx'
+   */
   layout?: string;
 }
